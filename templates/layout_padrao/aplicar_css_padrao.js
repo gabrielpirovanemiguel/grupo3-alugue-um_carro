886d2dbb081ea27f6f86f5bdf4086095b19950b6
Gabriel Pirovane Miguel <gabriel.balique@gmail.com>
2026-07-16 00:07:12 -0300

    feat: adicionando header e footer sendo adicionado por padrão

---
+++ templates/layout_padrao/aplicar_css_padrao.js
@@ -0,0 +1,7 @@
+document.write(`
+    <link rel="stylesheet" href="/templates/reset.css">
+    <link rel="stylesheet" href="/templates/root.css">
+    <link rel="stylesheet" href="/templates/layout_padrao/header.css">
+    <link rel="stylesheet" href="/templates/layout_padrao/footer.css">
+    <link rel="stylesheet" href="/templates/layout_padrao/body.css">
+    `);
---
+++ templates/layout_padrao/aplicar_layout_padrao.js
@@ -0,0 +1,11 @@
+import {
+    htmlHeader,
+    htmlFooter
+} from './elementos_html.js'
+
+const header = document.getElementById('header');
+const footer = document.getElementById('footer');
+header.innerHTML = htmlHeader;
+footer.innerHTML = htmlFooter;
+
+
