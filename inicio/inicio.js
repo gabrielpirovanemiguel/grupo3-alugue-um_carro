1aac1923d5e1bc1d7ad431eab13b8b923d9092db
Davi Almeida Matta da Silva <davialmeidamatta@gmail.com>
2026-07-17 16:31:35 -0300

    feat: adição da base dos destaques e js

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -45,3 +45,80 @@ async function carregarDadosIntroducao() {
 
 carregarDadosIntroducao();
 
+
+async function carregarDadosDestaques() {
+    try{
+        const resposta = await fetch('http://localhost:3001/carros');
+        const veiculos = await resposta.json();
+
+        const destaques_container = document.getElementById("card_destaques_container");
+
+        const veiculosDestaque = veiculos.slice(0,4);
+
+        let cardDestaques = '';
+
+        veiculosDestaque.forEach((carro,index) => {
+            let iconeCategoria = 'ph-video-camera'; 
+            let nomeCategoria = 'Filme';
+            let classeCorCategoria = 'tag_filme';
+
+            if (carro.categoria === 'desenho') {
+                iconeCategoria = 'ph-star'; 
+                nomeCategoria = 'Desenho';
+                classeCorCategoria = 'tag_desenho'; 
+            } else if (carro.categoria === 'série') {
+                iconeCategoria = 'ph-television'; 
+                nomeCategoria = 'Série';
+                classeCorCategoria = 'tag_serie'; 
+            }
+
+            const estaDisponivel = carro.status_disponibilidade === 'disponivel';  
+                
+                const tagStatus = estaDisponivel 
+                    ? `<span class="tag_disponivel">● Disponível</span>` 
+                    : `<span class="tag_indisponivel">● Indisponível</span>`;
+
+                const botaoAcao = estaDisponivel
+                    ? `<button class="botao_alugar">Alugar</button>`
+                    : `<button class="botao_indisponivel" disabled>Indisponível</button>` // desativar
+        
+
+        cardDestaques += `
+            <div class="card_veiculos">
+                <div class="card_imagem_destaques"> 
+                    <img src="${carro.url_imagem}" alt="${carro.nome}">
+                    <div class = "card_destaques_topo">
+                        <span class="tag_categoria ${classeCorCategoria}"><i class="ph ${iconeCategoria}"></i> ${nomeCategoria}</span> 
+                        ${tagStatus}
+                    </div>
+                </div>
+                
+                <div class="veiculos_destaques_info">
+                    <div class="texto_veiculos">
+                        <h3>${carro.nome}</h3>
+                        <p>${carro.universo_origem} </p>
+                    </div>
+                    <div class="preco_veiculo">
+                        <span>por dia</span>
+                        <h4>R$ ${carro.valor_aluguel_dia}</h4>
+                    </div>
+
+                    <div>
+                        ${botaoAcao}
+                        <button><i class="ph-ph-calendar-blank"></i></button>
+                    </div>
+                
+                </div>
+            </div>
+        `;
+            
+        });
+        destaques_container.innerHTML=cardDestaques
+    
+    } catch (error) {
+        console.error("Erro ao carregar os veículos:", erro);
+        
+    }
+}
+
+carregarDadosDestaques();
